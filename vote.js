b101c4bb9d5d085f068a8743af6f87d76f3c01c4
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2015-11-09 14:18:12 -0800

    object constructor, and all photo objects

---
+++ vote.js
@@ -0,0 +1,28 @@
+function Photo(name, path, info) {
+  this.name = name;
+  this.path = path;
+  this.info = info;
+  numVotes = 0;
+}
+
+var photoCollection = [
+  var angler = new Photo("Angler Fish", "img/angler-fish.jpg", "https://en.wikipedia.org/wiki/Anglerfish");
+  var hydro = new Photo("Arctic Hydromedusa", "img/arctic-hydromedusa.jpg", "https://en.wikipedia.org/wiki/Crossota_millsae");
+  var bioOct = new Photo("Bioluminescent Octopus", "img/bioluminescent-octopus.jpg", "https://en.wikipedia.org/wiki/Stauroteuthis_syrtensis");
+  var dumbo = new Photo("Dumbo Octopus", "img/dumbo-octopus.jpg", "https://en.wikipedia.org/wiki/Grimpoteuthis");
+  var fathead = new Photo("Fathead", "img/fathead.jpg", "https://en.wikipedia.org/wiki/Psychrolutes_microporos");
+  var snail = new Photo("Flamingo Tongue Snail", "img/flamingo-tongue-snail.jpg", "https://en.wikipedia.org/wiki/Flamingo_tongue_snail");
+  var laceBranch = new Photo("Golden Lace Nudibranch", "img/golden-lace-nudibranch.jpg", "https://en.wikipedia.org/wiki/Halgerda_terramtuentis");
+  var seadragon = new Photo("Leafy Seadragon", "img/leafy-seadragon.jpg", "https://en.wikipedia.org/wiki/Leafy_seadragon");
+  var lizardOct = new Photo("Lizard Island Octopus", "img/lizard-island-octopus.jpg", "https://en.wikipedia.org/wiki/Mimic_octopus");
+  var turtle = new Photo("Loggerhead Sea Turtle", "img/loggerhead-sea-turtle.jpg", "https://en.wikipedia.org/wiki/Loggerhead_sea_turtle");
+  var orthocanna = new Photo("Marrus Orthocanna", "img/marrus-orthocanna.jpg", "https://en.wikipedia.org/wiki/Marrus_orthocanna");
+  var walkingFish = new Photo("Mexican Walking Fish", "img/mexican-walking-fish.jpg", "https://en.wikipedia.org/wiki/Axolotl");
+  var norvegica = new Photo("Norvegica Jellyfish", "img/norvegica-jellyfish.jpg", "http://ocean.si.edu/ocean-photos/deep-red-jellyfish");
+  var fantasia = new Photo("Pink See-through Fantasia", "img/pink-see-through-fantasia.jpg", "http://www.ourbreathingplanet.com/pink-sea-through-fantasia/");
+  var angel = new Photo("Sea Angel", "img/sea-angel.jpg", "https://en.wikipedia.org/wiki/Sea_angel");
+  var nettles = new Photo("Sea Nettles", "img/sea-nettles.jpg", "https://en.wikipedia.org/wiki/Chrysaora_fuscescens");
+  var lobster = new Photo("Terrible Claw Lobster", "img/terrible-claw-lobster.jpg", "https://en.wikipedia.org/wiki/Dinochelus");
+  var squid = new Photo("Vampire Squid", "img/vampire-squid.jpg", "https://en.wikipedia.org/wiki/Vampire_squid");
+  var anemone = new Photo("Venus Flytrap Anemone", "img/venus-flytrap-anemone.jpg", "https://en.wikipedia.org/wiki/Venus_flytrap_sea_anemone");
+];
